98a5d9a43172ce018a245694b948c71b5e7209b0
ProgrammerPeasant <poland-fish@yandex.ru>
2025-06-13 18:35:59 +0300

    small fix

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -7,8 +7,6 @@ val springCloudVersion = "2023.0.1"
 dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-    //configurations.implementation.exclude(group: "org.springframework.boot", module: "spring-boot-starter-web")
-    //configurations.implementation.exclude(group: "org.springdoc", module: "springdoc-openapi-starter-webmvc-ui")
 }
 
 dependencyManagement {
